3af2accf85fed898e11af47d618b7c6331d7d751
beto1581 <nelsoncortezrueda1993@gmail.com>
2023-06-20 17:55:14 -0600

    Updates

---
+++ precache-manifest.f2f7c8acbcf8e86d77620841a51cc999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "438681ef43a74d89a475",
+    "url": "/todo-list/static/css/main.1ec57877.chunk.css"
+  },
+  {
+    "revision": "438681ef43a74d89a475",
+    "url": "/todo-list/static/js/main.438681ef.chunk.js"
+  },
+  {
+    "revision": "3bbfafec3fccff47e9b4",
+    "url": "/todo-list/static/js/1.3bbfafec.chunk.js"
+  },
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "d1131859bd053acfde1b19f859b4dca4",
+    "url": "/todo-list/index.html"
+  }
+];
